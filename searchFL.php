4a98ac6f5d6256bf0760c4c114289c2f8cf1399f
TensionVex <dal09Git>
2016-04-26 02:22:59 -0500

    butts

--- searchFL.php
+++ searchFL.php
@@ -6,10 +6,26 @@
 ob_implicit_flush();
 $fh = fopen('stores.txt', 'r');
 while ($line = fgets($fh)) {
-    $buttLink = "http://www.footlocker.com/storepickup/locations?action=getLocations&latlng={$line}&sku=65515010&size=12.0&qty=1&requestKey=632t7EF10C13FC96&rnd=14616521311994";
-    echo nl2br($buttLink . "\n");
+    $url = "http://www.footlocker.com/storepickup/locations?action=getLocations&latlng=" . trim($line) . "&sku=65515010&size=12.0&qty=1&requestKey=632t7EF10C13FC96&rnd=14616521311994";
+    echo $url;
+    $ch = curl_init($url);
+    $options = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_REFERER => "$url",
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_USERAGENT => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:45.0) Gecko/20100101 Firefox/45.0",
+            CURLOPT_HEADER => "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            CURLOPT_MAXREDIRS => 1,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_URL => $url
+            );
+    curl_setopt_array($ch, $options);
+    $buttcurl = curl_exec($ch);
+    curl_close($ch);
+    echo $buttcurl;
     ob_flush();
-    sleep(1);
+    sleep(5);
 }
+fclose($fh);
 echo "all done";
 ?>
