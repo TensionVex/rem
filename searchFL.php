d5b5358edc9c16ca97638a83aa7010ef3828bfb6
TensionVex <dal09Git>
2016-04-26 01:37:27 -0500

    butts

--- searchFL.php
+++ searchFL.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Seven
- * Date: 4/26/16
- * Time: 1:27 AM
- */
+
+// http://www.footlocker.com/storepickup/locations?action=getLocations&latlng=32.849351,-96.589017&sku=65515010&size=12.0&qty=1&requestKey=632t7EF10C13FC96&rnd=14616521311994
+// http://www.footlocker.com/storepickup/locations?action=getLocations&latlng={"$coord"}&sku=65515010&size=12.0&qty=1&requestKey=632t7EF10C13FC96&rnd=14616521311994
+
+$fh = fopen('stores.txt', 'r');
+while ($line = fgets($fh)) {
+    $butts = "butt" . $line;
+    echo $line;
+    flush();
+}
+echo "all done";
+?>
