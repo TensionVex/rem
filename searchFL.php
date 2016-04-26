20b67365bcda4d7a0fb9e8b1b9ad78723226ca17
TensionVex <dal09Git>
2016-04-26 02:34:42 -0500

    butts

--- searchFL.php
+++ searchFL.php
@@ -1,7 +1,6 @@
 <?php
 
 // http://www.footlocker.com/storepickup/locations?action=getLocations&latlng=32.849351,-96.589017&sku=65515010&size=12.0&qty=1&requestKey=632t7EF10C13FC96&rnd=14616521311994
-// http://www.footlocker.com/storepickup/locations?action=getLocations&latlng={"$coord"}&sku=65515010&size=12.0&qty=1&requestKey=632t7EF10C13FC96&rnd=14616521311994
 
 ob_implicit_flush();
 $fh = fopen('stores.txt', 'r');
