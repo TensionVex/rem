4a741f4380dbc0e53bffad8ba23d618e89e6a820
TensionVex <dal09Git>
2016-04-25 23:58:08 -0500

    butts

--- runalt.php
+++ runalt.php
@@ -1,6 +1,6 @@
 <?php
 // butts and stuff here
-//$butts = "043";
+//$butts = "044";
 echo "<pre>";
 
 function curlGet($url) {
@@ -36,7 +36,6 @@ function scrapeStore($url) { // address scraper for pages where the address is v
     $address = $buttStorePageXpath->query('//span[@itemprop="streetAddress"]');
     if ($address->length > 0) {
         $buttStore['address'] = "";
-//        $buttStore['address'] = strip_tags(trim($address->item(0)->nodeValue, "\r\t\n"),"<br>");
     }
 
     $city = $buttStorePageXpath->query('//span[@itemprop="addressLocality"]');
@@ -53,7 +52,6 @@ function scrapeStore($url) { // address scraper for pages where the address is v
     if ($zip->length > 0) {
         $buttStore['zip'] = trim($zip->item(0)->nodeValue);
     }
-//    echo "printing buttStore <br>";
     print_r($buttStore);
     return $buttStore;
 }
@@ -67,10 +65,8 @@ function scrapeLocs($url) { // scrape state pages for individual location URLs
     $storeLinks = $buttLocPageXpath->query('//td[@class="dotrow"]/a/@href'); //    //td[@class="dotrow"]/a/@href      //a[contains(@href,"view")]/@href
     foreach ($storeLinks as $storeLink) {
         scrapeStore("http://" . $buttDomain . $storeLink->nodeValue);
-//        array_push($buttLoc, "http://" . $buttDomain . $storeLink->nodeValue);
     }
-//    echo "printing buttLoc <br>";
-//    print_r($buttLoc);
+
     return $buttLoc;
 }
 
@@ -89,41 +85,28 @@ function scrapeFoot($url) { // scrape the first page for state page URLs
     $buttFootPage = curlGet($url);
     $buttFootPageXpath = returnXPathObject($buttFootPage);
 
-//    $locationLinks = $buttFootPageXpath->query('//a[contains(@href,"store/list_state/2174/Alab")]/@href'); // scraping query to get states
-    $locationLinks = $buttFootPageXpath->query('//a[contains(@href,"store/list_city")]/@href'); // alternate scraping query to get major Metro and California cities
+    $locationLinks = $buttFootPageXpath->query('//a[contains(@href,"store/list_state/2174/Alas")]/@href'); // scraping query to get states
+//    $locationLinks = $buttFootPageXpath->query('//a[contains(@href,"store/list_city")]/@href'); // alternate scraping query to get major Metro and California cities
     foreach ($locationLinks as $locationLink) {
         array_push($buttFoot, "http://" . $buttDomain . $locationLink->nodeValue);
     }
-//    print_r($buttFoot);
     return $buttFoot;
 }
 
 function passFoot($locs) { // take state results from first scrape and send to get locations
     foreach ($locs as $loc) {
         $buttMedio = scrapeLocs($loc);
-//        echo trim(implode($buttFinale));
-//        print_r($buttMedio);
         array_push($buttMedio, $loc->nodeValue);
     }
 }
 
-$firstUrl = "http://www.mystore411.com/store/list_state/2174/California/Foot-Locker-store-locations"; // California front page
-//$firstUrl = "http://www.mystore411.com/store/listing/2174/Foot-Locker-store-locations"; // All stores front page
-$buttStepZero = scrapeFoot($firstUrl);
-//print_r($buttStepZero);
-passFoot($buttStepZero);
-//$scrapeUrl = "http://www.mystore411.com/store/list_state/2174/Alabama/Foot-Locker-store-locations";
-//$buttStepOne = scrapeLocs($scrapeUrl);
-//passLocs($buttStepOne);
+$firstUrl = "http://www.mystore411.com/store/listing/2174/Foot-Locker-store-locations"; // All stores front page
+//$firstUrl = "http://www.mystore411.com/store/list_state/2174/California/Foot-Locker-store-locations"; // California front page
 
+$buttStepZero = scrapeFoot($firstUrl);
+passFoot($buttStepZero);
 
 echo nl2br("\n") . "this is the end of the butt";
-//echo implode($buttStepOne);
-//$buttFinale = scrapeStore($buttStepOne);
-//echo "'" . implode("'", $buttFinale) . "'";
-
-//$buttFinale = scrapeStore("http://www.mystore411.com/store/view/11720775/Foot-Locker-Anchorage");
-//echo "'" . implode("','", $buttFinale) . "'";
 
 echo "</pre>";
 ?>
